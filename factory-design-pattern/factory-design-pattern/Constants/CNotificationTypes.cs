8a9fa31f36c349e131e8d667594373bc0864fbd5
anshika3000 <anshika.2024it1203@kiet.edu>
2025-06-17 18:04:10 +0530

    1 :: Factory Design Pattern

---
+++ factory-design-pattern/factory-design-pattern/Constants/CNotificationTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_design_pattern.Constants
+{
+    class CNotificationTypes
+    {
+        public const string EmailNotification = "EMAIL";
+        public const string SMSNotification = "SMS";
+    }
+}
---
+++ factory-design-pattern/factory-design-pattern/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using factory_design_pattern.Constants;
+using factory_design_pattern.Factory;
+using factory_design_pattern.Interface;
+
+Console.WriteLine("Hello, World!");
+
+INotification notification = NotificationFactory.GetNotification(CNotificationTypes.EmailNotification);
+notification.Notify("Welcome to Factory Pattern in C#!");
+
+INotification sms = NotificationFactory.GetNotification(CNotificationTypes.SMSNotification);
+sms.Notify("OTP: 123456");
