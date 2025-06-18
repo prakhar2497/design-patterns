5535249d693902a3a5e33eda15e2b4cfb39bfa5f
Prakhar Mishra <prakhar.mishra1997@gmail.com>
2025-06-18 15:06:35 +0530

    1 :: factory design pattern ::
    Implemented with different approach for the factory by using dictonary not if-else or switch.

---
+++ factory-design-pattern/factory-design-pattern/Factory/NotificationFactory2.cs
@@ -0,0 +1,30 @@
+﻿using factory_design_pattern.Constants;
+using factory_design_pattern.Interface;
+using factory_design_pattern.Module;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factory_design_patterm.Factory
+{
+    public static class NotificationFactory2
+    {
+        public static readonly Dictionary<string, Func<INotification>> _creators = new Dictionary<string, Func<INotification>>
+        {
+            { CNotificationTypes.EmailNotification, () => new EmailNotification() },
+            { CNotificationTypes.SMSNotification, () => new SMSNotification() }
+        };
+
+        public static INotification GetNotification(string type)
+        {
+            if (_creators.TryGetValue(type, out var creator))
+            {
+                return creator();
+            }
+
+            throw new ArgumentException("Unsupported notification type");
+        }
+    }
+}
--- factory-design-pattern/factory-design-pattern/Program.cs
+++ factory-design-pattern/factory-design-pattern/Program.cs
@@ -1,12 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using factory_design_patterm.Factory;
 using factory_design_pattern.Constants;
 using factory_design_pattern.Factory;
 using factory_design_pattern.Interface;
 
 Console.WriteLine("Hello, World!");
 
+
+// Approach 1
 INotification notification = NotificationFactory.GetNotification(CNotificationTypes.EmailNotification);
 notification.Notify("Welcome to Factory Pattern in C#!");
 
 INotification sms = NotificationFactory.GetNotification(CNotificationTypes.SMSNotification);
 sms.Notify("OTP: 123456");
+
+// Approach 2
+INotification notification2 = NotificationFactory2.GetNotification(CNotificationTypes.EmailNotification);
+notification2.Notify("Welcome to Factory Pattern in C#!");
+
+INotification sms2 = NotificationFactory2.GetNotification(CNotificationTypes.SMSNotification);
+sms2.Notify("OTP: 123456");
